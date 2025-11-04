321a35a0ab2156d98e917773497f513a04cdd2f0
Khushi Agrawal <khushiagra011@gmail.com>
2025-11-04 16:51:49 -0700

    added comments for distribuor events and functions

--- contracts/DistributorActions.sol
+++ contracts/DistributorActions.sol
@@ -5,14 +5,16 @@ import "./ProductTypes.sol";
 /**
 This file lists everything a DISTRIBUTOR can do in the supply chain system.
 
-It includes the role name, possible errors, events (for record keeping),
-and functions that a distributor is allowed to call.
+The distributor is the link between the manufacturer and the retailer.
+
+They can receive goods, ship them to retailers, update shipping progress,
+and report if any products are damaged.
 **/
 
 interface DistributorActions is ProductTypes {
 
     // ----------- Role ----------------
-    // This is a unique code to check if someone is Distributor.
+    // This is a special code used to check if someone is a distributor.
     function DISTRIBUTOR_ROLE() external view returns (bytes32);
 
     // -------- ERRORS -------------
@@ -23,13 +25,20 @@ interface DistributorActions is ProductTypes {
     error NotCurrentHolder();   // The caller is not the current owner of the product
     error UnknownProduct(); // The product does not exist
 
+    // ---------- EVENTS ---------------
+    // These are like logs. They record what happened in blockchain
+
+
+    // This event is triggered when a manufacturer sends a product to a distributor.
     event ShippedToDistributor(
-        uint256 indexed productId,
-        address indexed fromFactoryOrOwner,
+        uint256 indexed productId,  // unique product id for the product being shipped
+        address indexed fromFactoryOrOwner, // 
         address indexed distributor,
         uint32 shipDate
     );
 
+
+    // This event is triggered when a distributor prepares a shipment to a retailer.
     event RetailShipmentDeclared(
         uint256 indexed productId,
         address indexed distributor,
@@ -38,11 +47,17 @@ interface DistributorActions is ProductTypes {
         bytes32 waybillHash
     );
 
+    // This event logs any shipping updates — for example, when goods leave a warehouse
+    // or reach a delivery hub.
+
     event ShippingStatusUpdated(
         uint256 indexed productId,
         bytes32 trackingHash,
         uint32 updatedAt
     );
+
+
+    // This event logs when a distributor reports a damaged product.
 
     event DamagedReported(
         uint256 indexed productId,
@@ -50,6 +65,9 @@ interface DistributorActions is ProductTypes {
         uint32 reportedAt
     );
 
+    // --------------- Functions ----------------
+
+    
     function batchTransferToDistributor(
         uint256[] calldata productIds,
         address distributor,
