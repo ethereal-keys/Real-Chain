f41a3363c2ed3fcdd5f183798c9fb19dd977c997
Tharanath <tharanat@asu.edu>
2025-11-05 00:30:50 -0700

    Tharanath | Created ProductTypes file

---
+++ contracts/ProductTypes.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.28;
+
+
+/**
+This file lists everything about the PRODUCT in the supply chain system.
+
+It includes the structure, possible errors, events (for record keeping),
+and functions that is allowed to be called on the PRODUCT.
+**/
