630834b9c53c737bb6161c75cb08a427f1fc4423
Tharanath <tharanat@asu.edu>
2025-11-05 01:06:16 -0700

    Tharanath | Add roles and mapping

--- contracts/ProductTypes.sol
+++ contracts/ProductTypes.sol
@@ -27,3 +27,11 @@ struct Product {
 }
 
 mapping(uint256 => Product) public products;
+
+bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+bytes32 public constant MANUFACTURER_ROLE = keccak256("MANUFACTURER_ROLE");
+bytes32 public constant DISTRIBUTOR_ROLE  = keccak256("DISTRIBUTOR_ROLE");
+bytes32 public constant RETAILER_ROLE     = keccak256("RETAILER_ROLE");
+bytes32 public constant QC_ROLE           = keccak256("QC_ROLE");
+
+mapping(address => mapping(bytes32 => bool)) public hasRole; // checks if the address has that role
