2483d1cf896711c75a4a64f0051fe8e51768a235
Tharanath <tharanat@asu.edu>
2025-11-05 00:48:54 -0700

    Tharanath | Add ProductStatus enum

--- contracts/ProductTypes.sol
+++ contracts/ProductTypes.sol
@@ -8,9 +8,17 @@ It includes the structure, possible errors, events (for record keeping),
 and functions that is allowed to be called on the PRODUCT.
 **/
 
+enum ProductStatus {
+    MANUFACTURED,
+    QUALITY_CHECKED,
+    IN_TRANSIT_TO_DISTRIBUTOR,
+    IN_STORE,
+    SOLD
+}
+
 struct Product {
     uint256 productId; // Used to index and identify product
-    string status; // Used to indicate current status of product in supply chain
+    ProductStatus status; // Used to indicate current status of product in supply chain
     address manufacturer; // Address of manufacturer
     uint256 manufactureTimestamp; // Time of manufacture, proof of authenticity
     address currentOwner; // Address of current owner
