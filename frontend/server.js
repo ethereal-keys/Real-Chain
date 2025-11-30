0c8c75c0898ebdaa29957c075e5bebb7e7579147
Matthew Groholski <mgrohols@asu.edu>
2025-11-30 14:22:19 -0700

    Readded frontend files.

---
+++ frontend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/verify/:productId", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "verify.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`),
+);
